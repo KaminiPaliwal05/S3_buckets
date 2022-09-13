a54a35709d62c390b974f12b14d45d5813b73859
Kamini <kaminip.codetown@gmail.com>
2022-09-13 13:15:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const { S3Client } = require('@aws-sdk/client-s3')
+const express = require('express')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+
+const app = express()
+
+const s3 = new S3Client()
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'some-bucket',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+app.post('/upload', upload.array('photos', 3), function(req, res, next) {
+  res.send('Successfully uploaded ' + req.files.length + ' files!')
+})
